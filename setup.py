760aea2f2b40aa7714a38e28fadb7b529c4017e9
George Mias <gmiaslab@gmail.com>
2017-12-21 19:00:39 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='ClassificaIO',
     packages=find_packages(),
-    version='1.0.0',
+    version='1.0.1',
     description='Graphical User Interface for machine learning classification algorithms from scikit-learn',
     long_description=long_description,
     author='G. Mias Lab',
