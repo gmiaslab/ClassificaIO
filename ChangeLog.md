b35f9994ce91c07271cbff7aeaa565034ecb5b3e
raeufroushangar <roushnroushan19@gmail.com>
2018-07-24 14:16:32 -0400

    Updated to 1.1.4.1

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,4 +1,7 @@
 #  ClassificaIO Version ChangeLog
+## 1.1.4.1
+* Updated scikit-learn to 0.19.1 in setup.py
+  
 ## 1.1.4
 * Updated installation requirement in setup.py  
 
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='ClassificaIO',
     packages=find_packages(),
-    version='1.1.4',
+    version='1.1.4.1',
     description='Graphical User Interface for machine learning classification algorithms from scikit-learn',
     long_description=long_description,
     include_package_data=True,
@@ -20,7 +20,7 @@ setup(
     author_email='gmiaslab@gmail.com',
     license='MIT',
     url='https://github.com/gmiaslab/ClassificaIO',
-    download_url='https://github.com/gmiaslab/ClassificaIO/archive/1.1.4.tar.gz',
+    download_url='https://github.com/gmiaslab/ClassificaIO/archive/1.1.4.1.tar.gz',
     keywords=['machine learning', 'classification','bioinformatics'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
@@ -39,8 +39,8 @@ setup(
     install_requires=[
         'Pillow>=5.2.0',
         'pandas>=0.23.3',
+        'scikit-learn==0.19.1',
         'numpy==1.14.5',
-        'scikit-learn>=0.19.2',
         'scipy>=1.1.0'],
     zip_safe=False
 )
